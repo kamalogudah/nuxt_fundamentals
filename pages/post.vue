00e68c78dcbef8e5ef47fe55c04f35c3cb66c4ea
kamalogudah <mcpaul2058@gmail.com>
2019-02-18 00:24:39 +0300

    Create Application Pages

---
+++ pages/post.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <article>
+      <h1 class="title">What is Chapo Ndondo</h1>
+      <p>Damn that is a crazy food for the gods hommie</p>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+  line-height: 1.5;
+}
+article * {
+  margin-bottom: 1rem;
+}
+aside {
+  min-width: 280px;
+  max-width: 280px;
+  padding-left: 2rem;
+}
+.title {
+  font-size: 2rem;
+}
+</style>
+ 
